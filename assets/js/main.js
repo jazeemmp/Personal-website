bc8d70f421a0dfeca66039c2440550cc1a1e2719
Mohammed Jazeem Mp <99042128+jazeemmp@users.noreply.github.com>
2022-02-06 05:07:48 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: MyResume - v4.7.0
-* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
